25957db98aaf0c3bc57893234ca81640e83bb15a
alberto <albertocarobarrera@gmail.com>
2024-02-08 00:24:36 +0100

    Proyecto Angular Peliculas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {AppHeaderComponent} from './app-header/app-header.component';
+
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
